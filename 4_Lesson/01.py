0bb8950d89219f6f38d8058c1804d56cad9dcb42
Zeynep-I <zisanowa@gmail.com>
2025-01-16 11:50:48 +0500

    1.0.3 commit

--- 4_Lesson/01.py
+++ 4_Lesson/01.py
@@ -2,4 +2,4 @@ height = int(input("What is your height in cm?"))
 if height >= 120:
     print("You can ride the rollercoster.")
 else:
-    print("Soory, you have to grow taller before you can ride.")
+    print("Soory, you have to grow taller before you can ride.") 
