1cb6353ccf7c1f4399af0bb20807d109f36616e8
Zeynep-I <zisanowa@gmail.com>
2025-01-16 11:52:33 +0500

    1.0.4 commit

--- 3_Lesson/01.py
+++ 3_Lesson/01.py
@@ -1,3 +1,3 @@
 num = int(input("Iki belgili san girizin:\n"))
 print(f"{num} -yn 1-nji sifri: {num // 10 }")
-print(f"{num} -yn 2-nji sifri: {num % 10 }")
+print(f"{num} -yn 2-nji sifri: {num % 10 }") 
